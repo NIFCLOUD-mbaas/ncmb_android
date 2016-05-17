4cafbbe29662b93d1c485a97415fb2fa1d37db3a
RaccoonDog <RaccoonDog@example.com>
2016-05-17 17:27:52 +0900

    fix NCMB.initialize argument for typo

--- example/SNSTest/facebooksample/src/main/java/com/nifty/cloud/mb/facebooksample/MainActivity.java
+++ example/SNSTest/facebooksample/src/main/java/com/nifty/cloud/mb/facebooksample/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NCMB.initialize(
-                this.getApplication(),
+                this.getApplicationContext(),
                 "YOUR_APPLICATION_KEY",
                 "YOUR_CLIENT_KEY"
         );
