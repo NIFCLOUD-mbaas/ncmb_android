c8b573dd2648cb2e122f7ec527523aa7232c7ca9
HocN <hocnv.qn@gmail.com>
2016-11-24 16:25:44 +0900

    add feature get all keys (object)

--- ncmb-core/src/main/java/com/nifty/cloud/mb/core/NCMBObject.java
+++ ncmb-core/src/main/java/com/nifty/cloud/mb/core/NCMBObject.java
@@ -328,4 +328,12 @@ public class NCMBObject extends NCMBBase{
         }
 
     }
+
+    /**
+     * return all keys from mFields of object
+     */
+    public JSONArray allKeys(){
+        JSONArray allKeys = this.mFields.names();
+        return allKeys;
+    }
 }
