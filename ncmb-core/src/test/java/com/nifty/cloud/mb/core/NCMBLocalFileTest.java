7053c69916c1cca778f58d2265d40cd13375b502
noguchi <noguchi.yutaka@stf.nifty.co.jp>
2017-04-27 15:23:06 +0900

    add test class for NCMBLocalFileTest

---
+++ ncmb-core/src/test/java/com/nifty/cloud/mb/core/NCMBLocalFileTest.java
@@ -0,0 +1,97 @@
+package com.nifty.cloud.mb.core;
+
+import com.squareup.okhttp.mockwebserver.MockWebServer;
+
+import junit.framework.Assert;
+
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import java.io.File;
+
+/**
+ * NCMBLocalFileTest class
+ */
+@RunWith(CustomRobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21, manifest = Config.NONE, shadows = {ShadowNCMBUser.class})
+public class NCMBLocalFileTest {
+
+    private boolean callbackFlag;
+
+    @Before
+    public void setup() throws Exception {
+
+        MockWebServer mServer = new MockWebServer();
+        mServer.setDispatcher(NCMBDispatcher.dispatcher);
+        mServer.start();
+
+        NCMB.initialize(RuntimeEnvironment.application.getApplicationContext(),
+                "appKey",
+                "cliKey",
+                mServer.getUrl("/").toString(),
+                null);
+        NCMBUser.currentUser = null;
+        Robolectric.getBackgroundThreadScheduler().pause();
+        Robolectric.getForegroundThreadScheduler().pause();
+
+        callbackFlag = false;
+    }
+
+    @After
+    public void teardown() {
+
+    }
+
+    /**
+     * TestCase
+     */
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void create() throws Exception {
+        String fileName = "Test";
+        File file = NCMBLocalFile.create(fileName);
+        Assert.assertEquals(fileName, file.getName());
+    }
+
+    @Test
+    public void writeFile() throws Exception {
+        String jsonString = "{\"key\":\"value\"}";
+
+        File file = NCMBLocalFile.create("Test");
+        Assert.assertFalse(file.exists());
+        NCMBLocalFile.writeFile(file, new JSONObject(jsonString));
+        Assert.assertTrue(file.exists());
+    }
+
+    @Test
+    public void readFile() throws Exception {
+        String jsonString = "{\"key\":\"value\"}";
+        File file = NCMBLocalFile.create("Test");
+        NCMBLocalFile.writeFile(file, new JSONObject(jsonString));
+
+        Assert.assertEquals(jsonString, NCMBLocalFile.readFile(file).toString());
+    }
+
+    @Test
+    public void deleteFile() throws Exception {
+        String jsonString = "{\"key\":\"value\"}";
+        File file = NCMBLocalFile.create("Test");
+
+        NCMBLocalFile.writeFile(file, new JSONObject(jsonString));
+        Assert.assertTrue(file.exists());
+
+        NCMBLocalFile.deleteFile(file);
+        Assert.assertFalse(file.exists());
+    }
+
+}
