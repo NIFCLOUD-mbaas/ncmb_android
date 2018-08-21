dfd620b09269e52e07bb76d1c46605c96a979d73
Cu Cong Can <cancc@vitalify.asia>
2018-08-21 15:54:03 +0700

    change package name for NCMBInstallationUtils and TokenCallback

--- ncmb-core/src/main/java/com/nifcloud/mbaas/core/NCMBInstallationUtils.java
+++ ncmb-core/src/main/java/com/nifcloud/mbaas/core/NCMBInstallationUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nifty.cloud.mb.core;
+package com.nifcloud.mbaas.core;
 
 import android.content.Context;
 import android.util.Log;
--- ncmb-core/src/main/java/com/nifcloud/mbaas/core/TokenCallback.java
+++ ncmb-core/src/main/java/com/nifcloud/mbaas/core/TokenCallback.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.nifty.cloud.mb.core;
+package com.nifcloud.mbaas.core;
 
 public interface TokenCallback extends CallbackBase {
     void done(String token, NCMBException e);
