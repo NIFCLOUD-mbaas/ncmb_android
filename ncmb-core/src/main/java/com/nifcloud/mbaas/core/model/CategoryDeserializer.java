d8b385db62976c5c82ee107e54d35d36b5fdca6f
TamHH <vfa.tamhh@gmail.com>
2019-01-29 12:37:13 +0900

    Add header and change target SDK for CategoryDeserializer class

--- ncmb-core/src/main/java/com/nifcloud/mbaas/core/model/CategoryDeserializer.java
+++ ncmb-core/src/main/java/com/nifcloud/mbaas/core/model/CategoryDeserializer.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2017-2018 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.nifcloud.mbaas.core.model;
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
@@ -15,6 +32,7 @@ import java.lang.reflect.Type;
 public class CategoryDeserializer implements JsonDeserializer<Category> {
 
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public Category deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
@@ -54,7 +72,6 @@ public class CategoryDeserializer implements JsonDeserializer<Category> {
 
         //Set default enableLights
         category.setEnableLights(true);
-
         JsonElement enableLights = jsonObject.get("enableLights");
         if (enableLights != null) {
             String code = enableLights.getAsString();
@@ -66,7 +83,6 @@ public class CategoryDeserializer implements JsonDeserializer<Category> {
 
         // Set default enableVibration
         category.setEnableVibration(true);
-
         JsonElement enableVibration = jsonObject.get("enableVibration");
         if (enableVibration != null) {
             String code = enableVibration.getAsString();
@@ -121,7 +137,7 @@ public class CategoryDeserializer implements JsonDeserializer<Category> {
 
     /**
      * Check lockscreenVisibility is in range
-     * @param lockscreenVisibility
+     * @param lockscreenVisibility value for lock screen Visibility
      * @return boolean
      */
     private boolean isLockScreenVisibility(int lockscreenVisibility) {
