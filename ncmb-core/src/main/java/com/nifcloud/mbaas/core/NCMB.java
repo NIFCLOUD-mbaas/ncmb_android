581a06343571b019a25dc79582d960f7894b162e
dinh.thuy.duong <dinh.thuy.duong@fujitsu.com>
2021-12-01 14:05:35 +0900

    Update version string

--- ncmb-core/src/main/java/com/nifcloud/mbaas/core/NCMB.java
+++ ncmb-core/src/main/java/com/nifcloud/mbaas/core/NCMB.java
@@ -30,7 +30,7 @@ public class NCMB {
     /**
      * Version of this SDK
      */
-    public static final String SDK_VERSION = "4.0.2";
+    public static final String SDK_VERSION = "4.0.3";
 
     /**
      * Prefix of keys in metadata for NCMB settings
