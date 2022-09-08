8d07b4d144409b0854d8d102fa0e3509322237f9
watanabe.tomoya <watanabe.tomoya@fjct-8010.local>
2022-09-08 10:19:45 +0900

    fix_local_error

---
+++ src/test/java/com/nifcloud/mbaas/core/NCMBInstallationUtilsTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017-2022 FUJITSU CLOUD TECHNOLOGIES LIMITED All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.nifcloud.mbaas.core;
+
+/**
+ * Test for NCMBInstallationTest
+ */
+@RunWith(CustomRobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21, manifest = "src/main/AndroidManifest.xml")
+public class NCMBInstallationUtilsTest {
+
+    private MockWebServer mServer;
+    private boolean callbackFlag;
+
+    @Before
+    public void setup() throws Exception {
+
+        //set application information
+        RobolectricPackageManager rpm = (RobolectricPackageManager) RuntimeEnvironment.application.getPackageManager();
+        PackageInfo packageInfo = new PackageInfo();
+        packageInfo.packageName = NCMBInstallationServiceTest.PACKAGE_NAME;
+        packageInfo.versionName = NCMBInstallationServiceTest.APP_VERSION;
+        packageInfo.applicationInfo = new ApplicationInfo();
+        packageInfo.applicationInfo.packageName = NCMBInstallationServiceTest.PACKAGE_NAME;
+        packageInfo.applicationInfo.name = NCMBInstallationServiceTest.APP_NAME;
+        rpm.addPackage(packageInfo);
+
+        //setup mocServer
+        mServer = new MockWebServer();
+        mServer.setDispatcher(NCMBDispatcher.dispatcher);
+        mServer.start();
+        String mockServerUrl = mServer.getUrl("/").toString();
+
+        //initialization
+        NCMB.initialize(RuntimeEnvironment.application.getApplicationContext(),
+                "applicationKey",
+                "clientKey",
+                mockServerUrl,
+                null);
+
+        MockitoAnnotations.initMocks(this);
+
+        ShadowLog.stream = System.out;
+
+        Robolectric.getBackgroundThreadScheduler().pause();
+        Robolectric.getForegroundThreadScheduler().pause();
+
+        callbackFlag = false;
+    }
+
+    @Test
+    public void updateInstallation_done_test() throws Exception {
+
+    }
+}
