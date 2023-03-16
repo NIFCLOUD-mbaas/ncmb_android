c13d96bcb5c2951ba19455969a9d41c4cb1c73fb
tanaka-kokoro <kokoro.tanaka@fujitsu.com>
2023-03-16 20:58:10 +0900

    Use params and reqParams

--- src/main/java/com/nifcloud/mbaas/core/NCMBUserService.java
+++ src/main/java/com/nifcloud/mbaas/core/NCMBUserService.java
@@ -827,14 +827,15 @@ public class NCMBUserService extends NCMBService {
      */
     protected RequestParams loginByNameParams(String userName, String password) throws NCMBException {
         try {
-            RequestParams params = new RequestParams();
-            params.url = mContext.baseUrl + "login";
-            params.type = NCMBRequest.HTTP_METHOD_POST;
+            RequestParams reqParams = new RequestParams();
+            reqParams.url = mContext.baseUrl + "login";
+            reqParams.type = NCMBRequest.HTTP_METHOD_POST;
 
             JSONObject params = new JSONObject();
             params.put("userName", userName);
             params.put("password", password);
-            return params;
+            reqParams.content = params.toString();
+            return reqParams;
         } catch (JSONException e) {
             throw new NCMBException(NCMBException.MISSING_VALUE, "userName/password required");
         }
@@ -912,16 +913,16 @@ public class NCMBUserService extends NCMBService {
      */
     protected RequestParams loginByMailParams(String mailAddress, String password) throws NCMBException {
         try {
-            RequestParams params = new RequestParams();
+            RequestParams reqParams = new RequestParams();
 
-            params.url = mContext.baseUrl + "login";
-            params.type = NCMBRequest.HTTP_METHOD_POST;
+            reqParams.url = mContext.baseUrl + "login";
+            reqParams.type = NCMBRequest.HTTP_METHOD_POST;
 
             JSONObject params = new JSONObject();
             params.put("mailAddress", mailAddress);
             params.put("password", password);
-
-            return params;
+            reqParams.content = params.toString();
+            return reqParams;
         } catch (JSONException e) {
             throw new NCMBException(NCMBException.MISSING_VALUE, "mailAddress/password required");
         }
